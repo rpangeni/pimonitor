15907d04f0d3895d49d4cd5969fe6f210f1dab68
rpangeni <r_pangeni@yahoo.com>
2017-01-27 19:35:23 +1100

    restored the lost refactored stuffs.

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -14,29 +14,62 @@ import cv2
 import shutil
 import emailapp
 import getpass
- 
+import logging
+import logging.config
+import os
 
-def startMonitoring(args):
+
+def setup_logging(default_path='pi_logging.json', default_level=logging.INFO, env_key='LOG_CFG'):
+    """Setup logging configuration
+
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+
+def getConf():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--conf", required=True,
+                    help="path to the JSON configuration file")
+    args = vars(ap.parse_args())
+
     # filter warnings, load the configuration and initialize the Dropbox
     # client
     warnings.filterwarnings("ignore")
     conf = json.load(open(args["conf"]))
-    gmail_password = getpass.getpass('Enter password for gmail account ')
-    client = None
+    return conf
 
+
+def getDropBoxClient(conf):
+    logger = logging.getLogger('pi_surveillance')
     if conf["use_dropbox"]:
         # connect to dropbox and start the session authorization process
         flow = DropboxOAuth2FlowNoRedirect(conf["dropbox_key"], conf["dropbox_secret"])
-        print ("[INFO] Authorize this application: {}".format(flow.start()))
+        print("[INFO] Authorize this application: {}".format(flow.start()))
         authCode = raw_input("Enter auth code here: ").strip()
 
         # finish the authorization and grab the Dropbox client
         (accessToken, userID) = flow.finish(authCode)
         client = DropboxClient(accessToken)
-        print ("[SUCCESS] dropbox account linked")
+        # print ("[SUCCESS] dropbox account linked")
+        logger.info("[SUCCESS] dropbox account linked")
+        return client
+    else:
+        return None
 
-        # initialize the camera and grab a reference to the raw camera capture
 
+def captureAndProcess(conf, client, gmail_password):
+    # initialize the camera and grab a reference to the raw camera capture
+    logger = logging.getLogger('pi_surveillance')
     camera = PiCamera()
     camera.resolution = tuple(conf["resolution"])
     camera.framerate = conf["fps"]
@@ -44,12 +77,11 @@ def startMonitoring(args):
 
     # allow the camera to warmup, then initialize the average frame, last
     # uploaded timestamp, and frame motion counter
-    print ("[INFO] warming up...")
+    logger.info("warming up...")
     time.sleep(conf["camera_warmup_time"])
     avg = None
     lastUploaded = datetime.datetime.now()
     motionCounter = 0
-
 
     # capture frames from the camera
     for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -66,7 +98,7 @@ def startMonitoring(args):
 
         # if the average frame is None, initialize it
         if avg is None:
-            print ("[INFO] starting background model...")
+            logger.info("starting background model...")
             avg = gray.copy().astype("float")
             rawCapture.truncate(0)
             continue
@@ -76,13 +108,13 @@ def startMonitoring(args):
         # frame and running average
         cv2.accumulateWeighted(gray, avg, 0.5)
         frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-            # threshold the delta image, dilate the thresholded image to fill
+        # threshold the delta image, dilate the thresholded image to fill
         # in holes, then find contours on thresholded image
         thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
-            cv2.THRESH_BINARY)[1]
+                               cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
         (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
+                                        cv2.CHAIN_APPROX_SIMPLE)
 
         # loop over the contours
         for c in cnts:
@@ -99,11 +131,11 @@ def startMonitoring(args):
         # draw the text and timestamp on the frame
         ts = timestamp.strftime("%Y%h%d-%H%M%S")
         cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-            0.35, (0, 0, 255), 1)
+                    0.35, (0, 0, 255), 1)
 
-            # check to see if the room is occupied
+        # check to see if the room is occupied
         if text == "Occupied":
             # check to see if enough time has passed between uploads
             if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
@@ -120,7 +152,7 @@ def startMonitoring(args):
                         cv2.imwrite(t.path, frame)
 
                         # upload the image to Dropbox and cleanup the tempory image
-                        print ("[UPLOAD] {}".format(ts))
+                        logger.info("[UPLOAD] {}".format(ts))
                         path = "{base_path}/{timestamp}.jpg".format(
                             base_path=conf["dropbox_base_path"], timestamp=ts)
                         client.put_file(path, open(t.path, "rb"))
@@ -129,16 +161,16 @@ def startMonitoring(args):
                         t = TempImage()
                         cv2.imwrite(t.path, frame)
                         shutil.copy2(t.path, '../images/image{}.jpg'.format(ts))
-                        emailapp.sendEmailUsingGmail(t.path, conf["email_to"], 'Security Image',  conf["gmail_user"], gmail_password)
+                        logger.info("Sending email with {} as attachment".format((ts)))
+                        emailapp.sendEmailUsingGmail(t.path, conf["email_to"], 'Security Image', conf["gmail_user"],
+                                                     gmail_password)
                         t.cleanup()
                     else:
-                                                                                   # write the image to temporary file
+                        # write the image to temporary file
                         t = TempImage()
                         cv2.imwrite(t.path, frame)
                         shutil.copy2(t.path, '../images/image{}.jpg'.format(ts))
                         t.cleanup()
-
-
 
                     # update the last uploaded timestamp and reset the motion
                     # counter
@@ -161,10 +193,10 @@ def startMonitoring(args):
         # clear the stream in preparation for the next frame
         rawCapture.truncate(0)
 
+
 if __name__ == '__main__':
-    # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-c", "--conf", required=True,
-        help="path to the JSON configuration file")
-    args = vars(ap.parse_args())
-    startMonitoring(args)
+    setup_logging()
+    conf = getConf()
+    gmail_password = getpass.getpass('Enter password for gmail account ')
+    client = getDropBoxClient(conf)
+    captureAndProcess(conf, client, gmail_password)
